3a9df5d0205f12e5cbdd9468e1840eac14dfe549
Bobby Lee <superbahbi@gmail.com>
2022-08-27 13:00:48 -0700

    rename invoice

--- web/src/types/interface.ts
+++ web/src/types/interface.ts
@@ -5,7 +5,7 @@ export interface OverviewProps {}
 export interface TransactionProps {}
 export interface AnalyticsProps {}
 export interface WalletsProps {}
-export interface InvoicesProps {}
+export interface InvoiceProps {}
 export interface AccountProps {}
 export interface SettingsProps {}
 
---
+++ web/src/views/Invoice.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "../layouts/Main";
+import { InvoiceProps } from "../types/interface";
+
+const Invoice: React.FC<InvoiceProps> = ({}) => {
+  return (
+    <>
+      <Main>Invoices</Main>
+    </>
+  );
+};
+export default Invoice;
