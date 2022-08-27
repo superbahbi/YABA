3f51abb40cee92dfd73577c29eb0f9b9d005aab6
Bobby Lee <superbahbi@gmail.com>
2022-08-27 15:26:13 -0700

    refactor navbar icon assest
    added new icon assests from heroicon

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-interface NavbarProps {}
+import { ReactComponent as IconAccount } from "../assets/icons/account.svg";
+import { ReactComponent as IconChart } from "../assets/icons/chart.svg";
+import { ReactComponent as IconHome } from "../assets/icons/home.svg";
+import { ReactComponent as IconInvoice } from "../assets/icons/invoice.svg";
+import { ReactComponent as IconSetting } from "../assets/icons/setting.svg";
+import { ReactComponent as IconWallet } from "../assets/icons/wallet.svg";
+import { NavbarProps } from "../types/interface";
 
 const Navbar: React.FC<NavbarProps> = () => {
   return (
-    <nav className="flex-col h-full w-10  lg:px-6 md:px-3 lg:mr-6 pt-4 lg:w-52 hidden md:flex">
+    <nav className="flex-col h-full w-10 lg:px-6 md:px-3 lg:mr-6 pt-4 lg:w-52 hidden md:flex">
       <Link
         className="text-sm uppercase font-bold p-4 px-0 hidden lg:block"
         to="/"
@@ -13,62 +19,58 @@ const Navbar: React.FC<NavbarProps> = () => {
       </Link>
       <ul className="flex flex-col list-none">
         <li className="items-center">
-          <Link className={"text-xs uppercase py-3 font-bold block "} to="/">
-            <i className={"fas fa-home mr-2 text-sm "}> </i>
+          <Link className={"flex text-sm py-3 font-light"} to="/">
+            <i className={"flex items-center mr-2"}>
+              <IconHome className="h-4" />
+            </i>
             <span className="hidden lg:inline-block">Overview</span>
           </Link>
         </li>
         <li className="items-center">
-          <Link
-            className={"text-xs uppercase py-3 font-bold block "}
-            to="/transaction"
-          >
-            <i className={"fas fa-chart-pie mr-2 text-sm "}> </i>
+          <Link className={"flex text-sm py-3 font-light"} to="/transaction">
+            <i className={"flex items-center mr-2"}>
+              <IconChart className="h-4" />
+            </i>
             <span className="hidden lg:inline-block">Transaction</span>
           </Link>
         </li>
         <li className="items-center">
-          <Link
-            className={"text-xs uppercase py-3 font-bold block "}
-            to="/analytics"
-          >
-            <i className={"fas fa-chart-pie mr-2 text-sm "}> </i>
+          <Link className={"flex text-sm py-3 font-light"} to="/analytics">
+            <i className={"flex items-center mr-2"}>
+              <IconChart className="h-4" />
+            </i>
             <span className="hidden lg:inline-block">Analytics</span>
           </Link>
         </li>
         <li className="items-center">
-          <Link
-            className={"text-xs uppercase py-3 font-bold block "}
-            to="/wallet"
-          >
-            <i className={"fas fa-wallet mr-2 text-sm "}> </i>
+          <Link className={"flex text-sm py-3 font-light"} to="/wallet">
+            <i className={"flex items-center mr-2"}>
+              <IconWallet className="h-4" />
+            </i>
             <span className="hidden lg:inline-block">Wallet</span>
           </Link>
         </li>
         <li className="items-center">
-          <Link
-            className={"text-xs uppercase py-3 font-bold block "}
-            to="/invoice"
-          >
-            <i className={"fas fa-receipt mr-2 text-sm "}> </i>
+          <Link className={"flex text-sm py-3 font-light"} to="/invoice">
+            <i className={"flex items-center mr-2"}>
+              <IconInvoice className="h-4" />
+            </i>
             <span className="hidden lg:inline-block">Invoice</span>
           </Link>
         </li>
         <li className="items-center">
-          <Link
-            className={"text-xs uppercase py-3 font-bold block "}
-            to="/account"
-          >
-            <i className={"fas fa-user mr-2 text-sm "}> </i>
+          <Link className={"flex text-sm py-3 font-light"} to="/account">
+            <i className={"flex items-center mr-2"}>
+              <IconAccount className="h-4" />
+            </i>
             <span className="hidden lg:inline-block">Account</span>
           </Link>
         </li>
         <li className="items-center">
-          <Link
-            className={"text-xs uppercase py-3 font-bold block "}
-            to="/settings"
-          >
-            <i className={"fas fa-wrench mr-2 text-sm "}> </i>
+          <Link className={"flex text-sm py-3 font-light"} to="/settings">
+            <i className={"flex items-center mr-2"}>
+              <IconSetting className="h-4" />
+            </i>
             <span className="hidden lg:inline-block">Settings</span>
           </Link>
         </li>
