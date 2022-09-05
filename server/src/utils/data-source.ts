4a0d6d21ad2bfbf36d0ab6b3018e7c86a55231f4
Bobby Lee <superbahbi@gmail.com>
2022-09-05 14:28:57 -0700

    fix: :package: update datasource with environmental variable

--- server/src/utils/data-source.ts
+++ server/src/utils/data-source.ts
@@ -1,11 +1,12 @@
+import "dotenv/config";
 import { DataSource } from 'typeorm';
 import { User } from '../entities/user.entity';
 export const AppDataSource = new DataSource({
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'v33219',
-    database: 'yaba',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
     type: 'postgres',
     synchronize: false,
     logging: false,
