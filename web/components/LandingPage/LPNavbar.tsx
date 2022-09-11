a2d7f25f6432a8f5441ad0aa8519dbfdb52bbc05
Elton Bautista <eltonbestaha@gmail.com>
2022-09-11 07:41:09 -0600

    Changed padding and a couple of other properties for mobile & large

--- web/components/LandingPage/LPNavbar.tsx
+++ web/components/LandingPage/LPNavbar.tsx
@@ -4,13 +4,13 @@ import { ILPNavbarProps } from "../../types/LPinterface";
 
 const LPNavbar: React.FC<ILPNavbarProps> = ({ setTheme, theme }) => {
   return (
-    <div className="navbar bg-base-100 z-10 sticky top-0 bg-none justify-center">
+    <div className="navbar bg-base-100 z-10 sticky top-0 bg-none justify-center lg:px-20">
       <div className="lg:max-w-screen-xl w-screen">
-        <div className="flex-1">
-          <h1 className="btn btn-ghost normal-case text-xl">yaba.</h1>
+        <div className="flex-1 pl-4 lg:pl-0">
+          <h1 className="btn btn-ghost normal-case text-xl lg:p-0">yaba.</h1>
         </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal p-0">
+        <div className="flex-none pr-4">
+          <ul className="menu menu-horizontal p-0 hidden md:flex">
             <li>
               <NextLink
                 className="btn btn-ghost normal-case text-sm text-accent"
@@ -71,6 +71,97 @@ const LPNavbar: React.FC<ILPNavbarProps> = ({ setTheme, theme }) => {
                   contact
                 </button>
               </NextLink>
+            </li>
+            <li>
+              <label className="swap swap-rotate">
+                <input
+                  type="checkbox"
+                  onChange={() => {
+                    setTheme(!theme);
+                  }}
+                />
+                <svg
+                  className="swap-on fill-current w-5 h-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                </svg>
+                <svg
+                  className="swap-off fill-current w-5 h-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                </svg>
+              </label>
+            </li>
+          </ul>
+
+          <ul className="menu menu-horizontal p-0 md:hidden">
+            <li tabIndex={0}>
+              <NextLink
+                className="btn btn-ghost normal-case text-sm text-accent"
+                href="/explore"
+              >
+                <button className="btn btn-ghost lowercase btn-xs sm:btn-sm md:btn-md lg:btn-md no-underline text-accent text-sm">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="inline-block w-5 h-5 stroke-current"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    ></path>
+                  </svg>
+                </button>
+              </NextLink>
+              <ul className="p-2 bg-base-100">
+                <li>
+                  <NextLink
+                    className="btn btn-ghost normal-case text-sm text-accent"
+                    href="/explore"
+                  >
+                    <button className="btn btn-ghost lowercase btn-xs sm:btn-sm md:btn-md lg:btn-md no-underline text-accent text-sm">
+                      explore
+                    </button>
+                  </NextLink>
+                </li>
+                <li>
+                  <NextLink
+                    className="btn btn-ghost normal-case text-sm text-accent"
+                    href="/explore"
+                  >
+                    <button className="btn btn-ghost lowercase btn-xs sm:btn-sm md:btn-md lg:btn-md no-underline text-accent text-sm">
+                      login
+                    </button>
+                  </NextLink>
+                </li>
+                <li>
+                  <NextLink
+                    className="btn btn-ghost normal-case text-sm text-accent"
+                    href="/explore"
+                  >
+                    <button className="btn btn-ghost lowercase btn-xs sm:btn-sm md:btn-md lg:btn-md no-underline text-accent text-sm">
+                      sign up
+                    </button>
+                  </NextLink>
+                </li>
+                <li>
+                  <NextLink
+                    className="btn btn-ghost normal-case text-sm text-accent"
+                    href="/contact"
+                  >
+                    <button className="btn btn-ghost lowercase btn-xs sm:btn-sm md:btn-md lg:btn-md no-underline text-accent text-sm">
+                      contact
+                    </button>
+                  </NextLink>
+                </li>
+              </ul>
             </li>
             <li>
               <label className="swap swap-rotate">
