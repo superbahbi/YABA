cf0e3a041872c5131f618ed6a4ffd526e9478917
Elton Bautista <eltonbestaha@gmail.com>
2022-09-11 10:22:49 -0600

    Componentized LPBodyItem

--- web/components/LandingPage/LPBody.tsx
+++ web/components/LandingPage/LPBody.tsx
@@ -1,95 +1,37 @@
+import Image from "next/image";
 import React from "react";
 import { ILPBodyProps } from "../../types/LPinterface";
+import LPBodyItem from "./LPBodyItem";
 
 const LPBody: React.FC<ILPBodyProps> = () => {
   return (
     <section className="grid justify-items-center pt-12 px-4 lg:px-20">
       <div className="grid max-w-screen-xl w-full min-h-screen">
         <div className="grid lg:grid-cols-2 lg:gap-10 lg:gap-y-32">
-          <h1 className="text-4xl lg:text-6xl pb-14 h-fit lg:col-span-2">
+          <h1 className="text-4xl lg:text-6xl pb-14 h-fit lg:col-span-2 lg:sticky lg:top-16 z-10">
             we do it all.
           </h1>
-          <div className="flex items-end mt-10 md:mt-0">
-            <h1 className="text-6xl lg:text-9xl ">
-              <span className="h-fit">01</span>
-            </h1>
-          </div>
-          <div className="mb-10 md:mb-0 lg:w-[90%] lg:grid items-end h-[50vh]">
-            <h2 className="text-4xl py-4 lg:py-0 lg:text-7xl lg:mb-5">
-              brand experience
-            </h2>
-            <p className="text-sm md:text-base ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-              dignissimos ex laborum aliquid ducimus doloremque alias earum
-              inventore distinctio magnam! Fuga possimus, adipisci quibusdam
-              obcaecati neque ullam hic natus ipsa omnis unde doloremque
-              voluptatum nobis earum veniam ipsam a modi voluptatem laborum ut?
-              Quam veniam ex tempore non? Ea, a.
-            </p>
-
-            <p className="text-sm md:text-base ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-              dignissimos ex laborum aliquid ducimus doloremque alias earum
-              inventore distinctio magnam! Fuga possimus, adipisci quibusdam
-              obcaecati neque ullam hic natus ipsa omnis unde doloremque
-              voluptatum nobis earum veniam ipsam a modi voluptatem laborum ut?
-              Quam veniam ex tempore non? Ea, a.
-            </p>
-          </div>
-          <div className="flex items-end mt-10 md:mt-0">
-            <h1 className="text-6xl lg:text-9xl ">
-              <span className="h-fit">02</span>
-            </h1>
-          </div>
-          <div className="mb-10 md:mb-0 lg:w-[90%] lg:grid items-end h-[50vh]">
-            <h2 className="text-4xl py-4 lg:py-0 lg:text-7xl lg:mb-5">
-              brand experience
-            </h2>
-            <p className="text-sm md:text-base ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-              dignissimos ex laborum aliquid ducimus doloremque alias earum
-              inventore distinctio magnam! Fuga possimus, adipisci quibusdam
-              obcaecati neque ullam hic natus ipsa omnis unde doloremque
-              voluptatum nobis earum veniam ipsam a modi voluptatem laborum ut?
-              Quam veniam ex tempore non? Ea, a.
-            </p>
-
-            <p className="text-sm md:text-base ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-              dignissimos ex laborum aliquid ducimus doloremque alias earum
-              inventore distinctio magnam! Fuga possimus, adipisci quibusdam
-              obcaecati neque ullam hic natus ipsa omnis unde doloremque
-              voluptatum nobis earum veniam ipsam a modi voluptatem laborum ut?
-              Quam veniam ex tempore non? Ea, a.
-            </p>
-          </div>
-          <div className="flex items-end mt-10 md:mt-0">
-            <h1 className="text-6xl lg:text-9xl ">
-              <span className="h-fit">03</span>
-            </h1>
-          </div>
-          <div className="mb-10 md:mb-0 lg:w-[90%] lg:grid items-end h-[50vh]">
-            <h2 className="text-4xl py-4 lg:py-0 lg:text-7xl lg:mb-5">
-              brand experience
-            </h2>
-            <p className="text-sm md:text-base ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-              dignissimos ex laborum aliquid ducimus doloremque alias earum
-              inventore distinctio magnam! Fuga possimus, adipisci quibusdam
-              obcaecati neque ullam hic natus ipsa omnis unde doloremque
-              voluptatum nobis earum veniam ipsam a modi voluptatem laborum ut?
-              Quam veniam ex tempore non? Ea, a.
-            </p>
-
-            <p className="text-sm md:text-base ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-              dignissimos ex laborum aliquid ducimus doloremque alias earum
-              inventore distinctio magnam! Fuga possimus, adipisci quibusdam
-              obcaecati neque ullam hic natus ipsa omnis unde doloremque
-              voluptatum nobis earum veniam ipsam a modi voluptatem laborum ut?
-              Quam veniam ex tempore non? Ea, a.
-            </p>
-          </div>
+          <LPBodyItem
+            heading="brand experience"
+            imgSrc="https://i.postimg.cc/mZFsrnbZ/i-Phone-13-Pro.png"
+            num={"01."}
+            textOne=""
+            textTwo=""
+          />
+          <LPBodyItem
+            heading="brand experience"
+            imgSrc="https://i.postimg.cc/mZFsrnbZ/i-Phone-13-Pro.png"
+            num={"02."}
+            textOne=""
+            textTwo=""
+          />
+          <LPBodyItem
+            heading="brand experience"
+            imgSrc="https://i.postimg.cc/mZFsrnbZ/i-Phone-13-Pro.png"
+            num={"03."}
+            textOne=""
+            textTwo=""
+          />
         </div>
       </div>
     </section>
---
+++ web/components/LandingPage/LPBodyItem.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import React from "react";
+import NextLink from "next/link";
+
+export interface ILPBodyItemProps {
+  num: string;
+  imgSrc: string;
+  heading: string;
+  textOne: string;
+  textTwo?: string;
+}
+
+const LPBodyItem: React.FC<ILPBodyItemProps> = ({
+  heading,
+  imgSrc,
+  num,
+  textOne,
+  textTwo,
+}) => {
+  return (
+    <>
+      <div className="grid items-end mt-10 md:mt-0 grid-cols-3">
+        <h1 className="text-6xl lg:text-9xl text-accent">
+          <span className="h-fit">{num}</span>
+        </h1>
+        <div className="col-span-2 grid justify-items-center">
+          <Image src={imgSrc} alt="test" width="200" height="400" />
+        </div>
+      </div>
+      <div className="mb-10 md:mb-0 lg:w-[90%] grid items-end lg:h-[50vh]">
+        <h2 className="text-4xl py-4 lg:py-0 lg:text-7xl">{heading}</h2>
+        <p className="text-sm md:text-base pb-6 lg:pb-0 self-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+          dignissimos ex laborum aliquid ducimus doloremque alias earum
+          inventore distinctio magnam! Fuga possimus, adipisci quibusdam
+          obcaecati neque ullam hic natus ipsa omnis unde doloremque voluptatum
+          nobis earum veniam ipsam a modi voluptatem laborum ut? Quam veniam ex
+          tempore non? Ea, a.
+          {textOne}
+        </p>
+
+        <NextLink className="link no-underline " href="/account">
+          <button className="btn btn-primary w-1/2 lowercase btn-xs sm:btn-sm md:btn-md lg:btn-md justify-self-center">
+            Get Started
+          </button>
+        </NextLink>
+      </div>
+    </>
+  );
+};
+export default LPBodyItem;
