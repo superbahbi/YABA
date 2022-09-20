46a78428724a615381b510ef8ccd6d2762048d74
Bobby Lee <superbahbi@gmail.com>
2022-09-19 21:11:19 -0700

    feat: :card_file_box: added forgotPassword and resetPassword controller

--- server/src/controllers/auth.controllers.ts
+++ server/src/controllers/auth.controllers.ts
@@ -3,6 +3,8 @@ import { validationResult } from 'express-validator';
 import { CookieOptions, Request, Response } from "express";
 import { Prisma, PrismaClient } from '@prisma/client'
 import argon2 from "argon2"
+import { v4 as uuidv4 } from 'uuid';
+import { sendEmail } from "../utils/sendEmail";
 const prisma = new PrismaClient()
 
 const cookiesOptions: CookieOptions = {
@@ -118,9 +120,101 @@ const register = async (req: Request, res: Response) => {
 
   return;
 };
+/*
+  * @route   POST /api/auth/forgot-password
+  * @desc    Forgot password
+  * @access  Public
+  * @params  email
+  * @return  void
+  * @errors  400, 500
+*/
+const forgotPassword = async (req: Request, res: Response) => {
+  const { email } = req.body;
+  // verify user input data
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ data: { error: errors.array() } });
+
+  // Check if user exists in database
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email.toLowerCase(),
+    }
+  })
+
+  // Check if user exist
+  if (!user) {
+    return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] });
+  }
+
+  // TODO send email with reset link
+  const token = uuidv4();
+  await prisma.resetPasswordToken.create({
+    data: {
+      token: token,
+      userId: user.id,
+    }
+  })
+  await sendEmail(user.email,
+    `<a href="http://localhost:3000 /change-password/${token}">reset password</a>`
+  )
+  return res.status(200).json({ status: "success" });
+};
+
+/*
+  * @route   POST /api/auth/change-password 
+  * @desc    Change password
+  * @access  Public
+  * @params  token, password
+  * @return  void
+  * @errors  400, 500
+*/
+const resetPassword = async (req: Request, res: Response) => {
+  const { token, password } = req.body;
+  // verify user input data
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ data: { error: errors.array() } });
+
+  // Check if user exists in database
+  const resetPasswordToken = await prisma.resetPasswordToken.findUnique({
+    where: {
+      token: token,
+    }
+  })
+
+  // Check if user exist
+  if (!resetPasswordToken) {
+    return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] });
+  }
+
+  // hashing password
+  const hashedPassword = await argon2.hash(password)
+  try {
+    // Check if user exists in database
+    const user: Prisma.UserCreateInput = await prisma.user.update({
+      where: {
+        id: resetPasswordToken.userId,
+      },
+      data: {
+        password: hashedPassword,
+      },
+    })
+    return res.status(200).json({ status: "success", data: { user } });
+  }
+  catch (error) {
+    return res.status(400).json({ status: "error", data: { error } });
+  }
+
+}
+/*
+  * @route   POST /api/auth/logout
+  * @desc    Logout user
+  * @access  Private
+  * @params  void
+  * @return  void
+*/
 const logout = (res: Response) => {
   res.cookie('access_token', '', { maxAge: 1 });
   res.cookie('refresh_token', '', { maxAge: 1 });
   res.cookie('logged_in', '', { maxAge: 1 });
 };
-export default { login, register, logout };
+export default { login, register, forgotPassword, resetPassword, logout };
