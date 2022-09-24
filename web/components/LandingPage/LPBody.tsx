1923ab61ecca3badd3a19d93268f9840c3f3c19e
Elton Bautista <eltonbestaha@gmail.com>
2022-09-24 07:31:14 -0600

    Worked on text

--- web/components/LandingPage/LPBody.tsx
+++ web/components/LandingPage/LPBody.tsx
@@ -6,28 +6,28 @@ import yabaTwo from "/assets/images/yaba-login.png";
 
 const LPBody: React.FC<ILPBodyProps> = () => {
   return (
-    <section className="grid justify-items-center pt-12 px-4 lg:px-20">
+    <section className="grid justify-items-center py-12 md:py-32 px-4 lg:px-20">
       <div className="grid max-w-screen-xl w-full min-h-screen">
         <div className="grid lg:grid-cols-2 lg:gap-10 lg:gap-y-32">
-          <h1 className="text-4xl lg:text-6xl pb-14 h-fit lg:col-span-2 lg:sticky lg:top-2 z-10">
-            we do it all.
-          </h1>
+          <h2 className="text-4xl lg:text-6xl pb-14 h-fit lg:col-span-2 lg:sticky lg:top-2 lg:z-10 uppercase">
+            We do it all.
+          </h2>
           <LPBodyItem
-            heading="purchase guarantee"
+            heading="Purchase guarantee."
             imgSrc="https://i.postimg.cc/mZFsrnbZ/i-Phone-13-Pro.png"
             num={"01."}
             textOne=""
             textTwo=""
           />
           <LPBodyItem
-            heading="access all accounts"
+            heading="Access all accounts."
             imgSrc={yabaTwo}
             num={"02."}
             textOne=""
             textTwo=""
           />
           <LPBodyItem
-            heading="experience sexperience"
+            heading="Experience greatness."
             imgSrc={yabaOne}
             num={"03."}
             textOne=""
