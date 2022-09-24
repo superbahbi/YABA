5f4e69c81d039e61cced40b18aaff9cdedea7ee7
Elton Bautista <eltonbestaha@gmail.com>
2022-09-24 07:31:05 -0600

    Changed color of Button

--- web/components/Button.tsx
+++ web/components/Button.tsx
@@ -18,35 +18,35 @@ const Button: React.FC<ButtonProps> = ({
       {link ? (
         <NextLink
           href={link}
-          className="relative inline-block px-8 py-3 overflow-hidden border border-indigo-600 group focus:outline-none focus:ring"
+          className="relative inline-block px-8 py-3 overflow-hidden border border-base-content group focus:outline-none focus:ring"
         >
-          <button className="relative inline-block px-8 py-3 overflow-hidden border border-indigo-600 group focus:outline-none focus:ring">
+          <button className="relative inline-block px-8 py-3 overflow-hidden border border-base-content group focus:outline-none focus:ring">
             <span
               className={`absolute ${inset} ${direction} ${
                 inset === "inset-x-0" ? "h-[2px]" : "w-[2px]"
-              } transition-all bg-indigo-600 ${
+              } transition-all bg-accent-focus ${
                 inset === "inset-x-0"
                   ? "group-hover:h-full"
                   : "group-hover:w-full"
-              } group-active:bg-indigo-500`}
+              } group-active:bg-accent-focus`}
             ></span>
-            <span className="relative text-sm font-medium text-indigo-600 transition-colors group-hover:text-white">
+            <span className="relative text-sm font-medium text-base-content transition-colors group-hover:text-white">
               {children}
             </span>
           </button>
         </NextLink>
       ) : (
-        <button className="relative inline-block px-8 py-3 overflow-hidden border border-indigo-600 group focus:outline-none focus:ring">
+        <button className="relative inline-block px-8 py-3 overflow-hidden border border-base-content group focus:outline-none focus:ring">
           <span
             className={`absolute ${inset} ${direction} ${
               inset === "inset-x-0" ? "h-[2px]" : "w-[2px]"
-            } transition-all bg-indigo-600 ${
+            } transition-all bg-accent-focus ${
               inset === "inset-x-0"
                 ? "group-hover:h-full"
                 : "group-hover:w-full"
-            } group-active:bg-indigo-500`}
+            } group-active:bg-accent-focus`}
           ></span>
-          <span className="relative text-sm font-medium text-indigo-600 transition-colors group-hover:text-white">
+          <span className="relative text-sm font-medium text-base-content transition-colors group-hover:text-white">
             {children}
           </span>
         </button>
