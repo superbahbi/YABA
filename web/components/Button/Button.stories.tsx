2c4249c143de2b0524bbffeb8e06d9a39e384d8d
Bobby Lee <superbahbi@gmail.com>
2022-10-16 19:25:52 +0000

    ci: trigger test

--- web/components/Button/Button.stories.tsx
+++ web/components/Button/Button.stories.tsx
@@ -10,28 +10,24 @@ export default {
 const Template: ComponentStory<typeof Button> = (args) => (
   <Button {...args}>Button</Button>
 );
-
 export const Left = Template.bind({});
 Left.args = {
   className: "w-52",
   direction: "left-0",
   inset: "inset-y-0",
 };
-
 export const Right = Template.bind({});
 Right.args = {
   className: "w-52",
   direction: "right-0",
   inset: "inset-y-0",
 };
-
 export const Top = Template.bind({});
 Top.args = {
   className: "w-52",
   direction: "top-0",
   inset: "inset-x-0",
 };
-
 export const Bottom = Template.bind({});
 Bottom.args = {
   className: "w-52",
