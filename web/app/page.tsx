373ec5fe18dfd7743d6f1192377c0a9f9a3b3ea1
Bobby Lee <superbahbi@gmail.com>
2022-10-28 00:35:48 +0000

    refactor: :art: change layout and page

---
+++ web/app/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React, { useEffect } from "react";
+// import LPBody from "../components/LandingPage/LPBody";
+import LPFooter from "../components/LandingPage/LPFooter";
+import LPHero from "../components/LandingPage/LPHero";
+import LPNavbar from "../components/LandingPage/LPNavbar";
+import { themeChange } from "theme-change";
+import LPFeature from "../components/LandingPage/LPFeature";
+import LPFaq from "../components/LandingPage/LPFaq";
+import LPStats from "../components/LandingPage/LPStats";
+import LPTeam from "../components/LandingPage/LPTeam";
+import LPSocialProof from "../components/LandingPage/LPSocialProof";
+
+export default function Home() {
+  useEffect(() => {
+    themeChange(false);
+    // 👆 false parameter is required for react project
+  }, []);
+  return (
+    <>
+      <div>
+        <LPNavbar />
+        <main>
+          <LPHero />
+          <LPFeature />
+          <LPSocialProof />
+          {/* <LPBody /> */}
+          {/* <LPEnd /> */}
+          <LPStats />
+          <LPTeam />
+          <LPFaq />
+        </main>
+      </div>
+      <LPFooter />
+    </>
+  );
+}
+
+// const Home: React.FC<ILPIndexProps> = () => {
+
+// };
