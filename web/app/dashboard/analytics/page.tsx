02c9c03cc8a3250b25854c9f27906b49f3452abb
Bobby Lee <superbahbi@gmail.com>
2022-10-30 22:29:47 +0000

    refactor: :art: migrate analytics to next13

---
+++ web/app/dashboard/analytics/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { IAnalyticsProps } from "@/types/interface";
+
+const Analytics: React.FC<IAnalyticsProps> = () => {
+  return (
+    <>
+      <p>Analytics</p>
+    </>
+  );
+};
+export default Analytics;
