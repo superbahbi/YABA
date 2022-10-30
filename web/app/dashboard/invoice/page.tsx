0951b7d72f034a677f9999bf4fc25d491429dc28
Bobby Lee <superbahbi@gmail.com>
2022-10-30 22:30:10 +0000

    refactor: :art: migrate invoice page to next13

---
+++ web/app/dashboard/invoice/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { IInvoiceProps } from "@/types/interface";
+
+const Invoice: React.FC<IInvoiceProps> = () => {
+  return (
+    <>
+      <p>Invoices</p>
+    </>
+  );
+};
+export default Invoice;
