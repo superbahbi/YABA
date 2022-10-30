a6bdd1b6e79d19a4905a777a8fb23746c481481a
Bobby Lee <superbahbi@gmail.com>
2022-10-30 22:29:04 +0000

    refactor: :art: migrate account page to next13

---
+++ web/app/dashboard/account/page.tsx
@@ -0,0 +1,103 @@
+import React, { useCallback, useState } from "react";
+import formurlencoded from "form-urlencoded";
+import { IAccountProps, IAccountsData } from "@/types/interface";
+import {
+  usePlaidLink,
+  PlaidLinkOnSuccess,
+  PlaidLinkOnEvent,
+  PlaidLinkOnExit,
+  PlaidLinkOptions,
+  PlaidLinkOnSuccessMetadata,
+} from "react-plaid-link";
+import { useQuery } from "@tanstack/react-query";
+import Card from "@/components/Card";
+
+const Account: React.FC<IAccountProps> = () => {
+  const [accounts, setAccounts] = useState<IAccountsData[]>();
+
+  const { data, isFetching } = useQuery(["linkToken"], () =>
+    fetch(process.env.NEXT_PUBLIC_API_URL + "/api/create_link_token").then(
+      (res) => res.json()
+    )
+  );
+  const onSuccess = useCallback<PlaidLinkOnSuccess>(
+    (publicToken: string, metadata: PlaidLinkOnSuccessMetadata) => {
+      const exchangePublicToken = async () => {
+        const exchangeResponse = await fetch(
+          process.env.NEXT_PUBLIC_API_URL + "/api/exchange_public_token",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: formurlencoded({
+              public_token: publicToken,
+              metadata: metadata,
+            }),
+          }
+        );
+        const { transactions, total_transactions } =
+          await exchangeResponse.json();
+        console.log("total_transactions", total_transactions);
+        console.log("transactions", transactions);
+        setAccounts(transactions);
+      };
+      exchangePublicToken();
+    },
+    []
+  );
+  const onEvent = useCallback<PlaidLinkOnEvent>((eventName, metadata) => {
+    console.log(eventName, metadata);
+  }, []);
+  const onExit = useCallback<PlaidLinkOnExit>((error, metadata) => {
+    console.log(error, metadata);
+  }, []);
+
+  const config: PlaidLinkOptions = {
+    token: data?.link_token,
+    onSuccess,
+    onEvent,
+    onExit,
+  };
+
+  const { open, ready } = usePlaidLink(config);
+
+  return (
+    <>
+      <div className="flex w-full">
+        <div className="h-screen m-4 p-4">
+          <Card title="Connect">
+            <button className="btn" onClick={() => open()} disabled={!ready}>
+              <div>{isFetching ? "Updating..." : "Add a bank account"}</div>
+            </button>
+          </Card>
+
+          {accounts && (
+            <Card title="Accounts">
+              <pre>{JSON.stringify(accounts, null, 2)}</pre>
+              {/* {accounts.map((account: IAccountsData) => (
+                  <div key={account.account_id}>
+                    <div className="flex flex-row">
+                      <h2 className="text-xl">
+                        {account.name}
+                        <span className="badge badge-lg m-2 capitalize">
+                          {account.subtype}
+                        </span>
+                      </h2>
+                      <h2 className="flex justify-center items-center text-xl">
+                        {new Intl.NumberFormat("en", {
+                          style: "currency",
+                          currency: "USD",
+                        }).format(account.balances.current)}
+                      </h2>
+                    </div>
+                  </div>
+                ))} */}
+            </Card>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+export default Account;
