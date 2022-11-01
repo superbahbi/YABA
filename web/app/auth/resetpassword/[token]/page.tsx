40a55163ca86fa4b42376ee5123b1b9be2347f64
Bobby Lee <superbahbi@gmail.com>
2022-11-01 00:58:27 +0000

    refactor: :art: add param props interface

--- web/app/auth/resetpassword/[token]/page.tsx
+++ web/app/auth/resetpassword/[token]/page.tsx
@@ -41,14 +41,11 @@ const schema = z.object({
     }
   }),
 });
-// TODO reset password validation
-// ex. min 8 characters, max 32 characters, 1 uppercase, 1 lowercase, 1 number, 1 special character
-// labels: enhancement, help wanted, good first issue
-export default function ResetPassword({
-  params,
-}: {
-  params: { token: string };
-}) {
+interface IParamsProps {
+  token: string;
+}
+
+export default function ResetPassword({ params }: { params: IParamsProps }) {
   const [response, setResponse] = React.useState<IResponseProps>({});
   const [, setErrors] = React.useState<IResponseProps>({});
   const router = useRouter();
