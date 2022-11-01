5d8ece8074303c92fc006e77f9e2b6644aa430d2
Bobby Lee <superbahbi@gmail.com>
2022-11-01 00:51:33 +0000

    refactor: :fire: remove eslint rule

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -7,7 +7,4 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
-  rules: {
-    "@next/next/no-html-link-for-pages": ["error", "web/pages/"],
-  },
 };
