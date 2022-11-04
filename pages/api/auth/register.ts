2660ae82fe7dc4632e77e322ba416fd426fd537b
Bobby Lee <superbahbi@gmail.com>
2022-11-04 01:56:49 +0000

    feat: :art: added register route

---
+++ pages/api/auth/register.ts
@@ -0,0 +1,67 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { validationResult } from 'express-validator';
+import { Prisma, PrismaClient } from '@prisma/client'
+import z from "zod"
+import argon2 from "argon2"
+
+const prisma = new PrismaClient()
+
+interface registerType {
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+}
+const schema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8).max(32),
+    firstName: z.string().min(2),
+    lastName: z.string().min(2),
+});
+/**
+* @route   POST /api/auth/register
+* @desc    Register user and send verification email to user email address 
+* @access  Public
+* @params  email, password, firstName, lastName
+* @return  void
+* @errors  400, 500
+*/
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    // Validate user input data
+    const validationResult = schema.safeParse(req.body);
+
+    // verify user input data
+    if (!validationResult.success) {
+        return res.status(400).json({ errors: validationResult.error });
+    }
+
+    // Get user input data
+    const { email, password, firstName, lastName }: registerType = req.body;
+    // hashing password
+    const hashedPassword = await argon2.hash(password)
+    try {
+        // Check if user exists in database
+        const user: Prisma.UserCreateInput = await prisma.user.create({
+            data: {
+                email,
+                firstName,
+                lastName,
+                password: hashedPassword,
+            },
+        })
+        const sanitizedUser = {
+            id: user.id,
+            email: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName,
+        }
+        return res.status(200).json({ data: { sanitizedUser } });
+    }
+    catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError)
+            if (error.code === 'P2002') {
+                return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+            }
+        return res.status(500).json({ errors: [{ msg: 'Server error' }] });
+    }
+}
