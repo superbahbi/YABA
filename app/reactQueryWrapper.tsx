e90b2d5e75e0879b0144cb97d1af017f4891c71e
Bobby Lee <superbahbi@gmail.com>
2022-11-05 20:44:48 +0000

    feat: :sparkles: added react query wrapper

---
+++ app/reactQueryWrapper.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+type Props = {
+  children: React.ReactNode;
+};
+const queryClient = new QueryClient();
+
+export default function ReactQueryWrapper({ children }: Props) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
