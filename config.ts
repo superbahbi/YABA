a1ccccc16939c41c98201188fef27655f826b913
Bobby Lee <superbahbi@gmail.com>
2022-11-07 03:56:48 +0000

    refactor: :fire: remove client url

--- config.ts
+++ config.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 export const config = {
     isProduction: process.env.NODE_ENV! === 'production',
-    clientUrl: process.env.CLIENT_URL!,
     baseDomain: process.env.BASE_DOMAIN!,
     accessTokenSecret: process.env.ACCESS_TOKEN_SECRET!,
     refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET!,
