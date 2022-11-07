a59c6de6fbce72c7063830c40d52b84d3272bfd7
Bobby Lee <superbahbi@gmail.com>
2022-11-07 03:57:39 +0000

    refactor: :art: rename react query wrapper to react query provider

--- utils/ReactQueryProvider.tsx
+++ utils/ReactQueryProvider.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ToastContainer } from "react-toastify";
 
 type Props = {
   children: React.ReactNode;
 };
 const queryClient = new QueryClient();
 
-export default function ReactQueryWrapper({ children }: Props) {
+export default function ReactQueryProvider({ children }: Props) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <ToastContainer />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
