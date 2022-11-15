40b27bf08eef54e3cbd69310dbe330ca0f2a44fe
Bobby Lee <superbahbi@gmail.com>
2022-11-15 03:55:39 +0000

    refactor: :art: remove api middleware validation

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,49 @@
 import { NextRequest, NextResponse } from "next/server";
 import { verifyAccessToken } from "./utils/cookies";
-// https://github.com/vercel/next.js/discussions/38227
-// This function can be marked `async` if using `await` inside
-export function middleware(req: NextRequest) {
+
+// const isAPIRoute = (pathname: string) => {
+//     return pathname.startsWith('/api');
+// }
+
+export async function middleware(req: NextRequest) {
     console.log("middleware")
+    // const { pathname } = req.nextUrl;
+    // if (isAPIRoute(pathname)) {
+    //     return NextResponse.redirect(new URL('/api/auth/unauthorized', req.url));
+    // }
+
     const accessToken = req.cookies.get("access")?.value
     if (!accessToken) {
+
         return NextResponse.redirect(new URL('/auth/login', req.url))
     }
     else {
-        const verifiedToken = verifyAccessToken(accessToken)
-        if (!verifiedToken) {
+        try {
+            const verifiedToken = await verifyAccessToken(accessToken)
+            // if expired or invalid token redirect to login
+            if (!verifiedToken) {
+                return NextResponse.redirect(new URL('/auth/login', req.url))
+            }
+        } catch (error) {
+            console.log(error)
             return NextResponse.redirect(new URL('/auth/login', req.url))
         }
+
         return NextResponse.next()
     }
 }
 
 // This function is called for every request that matches the `config.matcher` pattern
 export const config = {
-    matcher: ["/overview", "/transactions", "/analytics", "/wallet", "/invoice", "/accounts", "/settings", "/profile"],
+    matcher: [
+        "/overview",
+        "/transactions",
+        "/analytics",
+        "/wallet",
+        "/invoice",
+        "/accounts",
+        "/settings",
+        "/profile",
+        // "/api/:path*"
+    ],
 }
