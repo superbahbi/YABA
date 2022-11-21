2f31c96ceb9f8b40044de5d9d865a6f2a9b1f098
Bobby Lee <superbahbi@gmail.com>
2022-11-21 03:27:57 +0000

    refactor: :art: convert components to client render instead of server render

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import dayjs from "dayjs";
 import React, { useEffect, useState } from "react";
 import { IconChevronLeft, IconChevronRight } from "../assets/icons";
--- components/Graph/DougnutGraph.tsx
+++ components/Graph/DougnutGraph.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { IDougnutGraphProps } from "../../types/interface";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
